01680882b3b77e6b5d13bf0fc60c25fba2b21fde
Ajmal <ajmalshahabudeen@gmail.com>
2024-03-29 06:28:05 +0530

    Adding some deserts.

---
+++ components/Header/Nav.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
+export const Nav = () => {
+  return (
+    <main className="sticky top-0">
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>File</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>New Window</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Share</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Print</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+    </main>
+  );
+};
